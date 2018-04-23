5b0bd4e4a39f49281f5e76ea969eb47fa257b968
Bruno Almeida <dbrunob@gmail.com>
2018-04-23 00:47:47 -0400

    Event App created. Db Connection working. Models successfully scaffolded from Db

---
+++ EventApp/Models/EvenAppContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EventApp.Models
+{
+    public partial class EvenAppContext : DbContext
+    {
+        public virtual DbSet<Events> Events { get; set; }
+
+        public EvenAppContext(DbContextOptions<EvenAppContext>options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Events>(entity =>
+            {
+                entity.HasKey(e => e.EventId);
+
+                entity.Property(e => e.EventId).HasColumnName("eventId");
+
+                entity.Property(e => e.EventAddress)
+                    .HasColumnName("eventAddress")
+                    .HasMaxLength(350)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EventContact)
+                    .HasColumnName("eventContact")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EventDate)
+                    .HasColumnName("eventDate")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.EventDescription)
+                    .HasColumnName("eventDescription")
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EventLink)
+                    .HasColumnName("eventLink")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EventTitle)
+                    .HasColumnName("eventTitle")
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Picture).HasColumnType("image");
+
+                entity.Property(e => e.PictureType)
+                    .HasColumnName("pictureType")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ EventApp/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventApp.Models
+{
+    public partial class Events
+    {
+        public int EventId { get; set; }
+        public string EventTitle { get; set; }
+        public DateTime? EventDate { get; set; }
+        public byte[] Picture { get; set; }
+        public string PictureType { get; set; }
+        public string EventDescription { get; set; }
+        public string EventContact { get; set; }
+        public string EventLink { get; set; }
+        public string EventAddress { get; set; }
+    }
+}
